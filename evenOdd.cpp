f026eeec6bcde0f1a429a1bc9703ef2547ec8073
ravidangi7284 <93817949+ravidangi7284@users.noreply.github.com>
2021-11-11 22:16:35 +0530

    Create evenOdd.cpp

---
+++ evenOdd.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the number to check : ";
+    cin>>n;
+    if(n%2==0)
+        cout<<"Entered number is EVEN";
+    else
+        cout<<"Entered number is ODD";
+    
+    return 0;
+}
