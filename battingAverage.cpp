cf1db638bcf3e9ad888d91b54f13423256e68c58
ravidangi7284 <93817949+ravidangi7284@users.noreply.github.com>
2021-11-12 18:44:52 +0530

    Create battingAverage.cpp

---
+++ battingAverage.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    float sc, n, avg;
+    cout<<"Enter the score : ";
+    cin>>sc;
+    cout<<"Enter which times out : ";
+    cin>>n;
+    avg = sc/n;
+    cout<<"Batting Average : "<<avg;
+    
+    return 0;
+    
+}
