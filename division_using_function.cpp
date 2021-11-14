e4a00726de88a665f4ba73250bd8cd2a1a567a60
ravidangi7284 <93817949+ravidangi7284@users.noreply.github.com>
2021-11-14 20:55:18 +0530

    Create division_using_function.cpp

---
+++ division_using_function.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+float division(float dividend, float divisor)
+{
+    float ans = dividend / divisor;
+    return ans;
+}
+
+int main()
+{
+   float dividend, divisor;
+   cout<<"Enter dividend : ";
+   cin>>dividend;
+   cout<<"Enter divisor : ";
+   cin>>divisor;
+   cout<<dividend<<"/"<<divisor<<" : "<<division(dividend, divisor);
+    
+    return 0;
+}
